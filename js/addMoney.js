5d6cf7c4e3da26795d69070b8a133b6459fbe24f
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-09-20 04:08:18 +0600

    added the transaction history part and ongoing

--- js/addMoney.js
+++ js/addMoney.js
@@ -15,6 +15,15 @@ document.getElementById('btn-add-money')
             const newBalance = balance + addMoney;
 
             document.getElementById('account-balance').innerText = newBalance;
+
+            // add to transaction history
+            const p = document.createElement('p');
+            // template string, dynamically input set
+            p.innerText = `Added: ${addMoney} Tk. Total: ${newBalance}`;
+            console.log(p)
+
+            // should be a common function
+            document.getElementById('transaction-container').appendChild(p)
         }
         else{
             alert('Failed to proceed');
